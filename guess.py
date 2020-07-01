f8e850130a5cf4bd65116d898c67b843e5f92575
mviamonte <66328257+mviamonte@users.noreply.github.com>
2020-07-01 16:47:08 -0400

    Update guess.py

--- guess.py
+++ guess.py
@@ -1,12 +1,12 @@
-###Guess game with three attempts
-number = 40
-counter = 1
-guess = int(input ("Introduce a number:"))
-if number !=guess:
-  if counter !=3:
-   guess = int(input ("Introduce another number:"))
-   counter=counter+1
-  else:
-    print ("You ran out of attemtps") 
-else:
-  print (f"Y")
+##Guess game with three attempts
+secret_number = 10 # The secret number has to be defined
+counter = 0 #Counter startint point
+counter_limit=3 # The limit of the counter
+while counter < counter_limit: # The conditions of maximum three attempts
+    guess = int(input ("Introduce a number:"))
+    counter = counter + 1
+    if secret_number == guess:
+      print (f"You guess, the number is {secret_number}")
+      break
+else: #The else condition to show user the lack of attemtps
+  print ("You ran out of attempts")
