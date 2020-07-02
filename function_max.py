6120ee6e4cee2b545970e39d0294e87149ec2c86
mviamonte <66328257+mviamonte@users.noreply.github.com>
2020-07-02 16:14:10 -0400

    Update function_max.py

--- function_max.py
+++ function_max.py
@@ -1,5 +1,5 @@
 #Write a function that detects the maximum of two numbers
-def maximum (num1,num2):
+def maximum (num1,num2):#You need to define the parameters within the function
     """ This function determines the greater value 
     of two parameters """
     if num1 > num2:
